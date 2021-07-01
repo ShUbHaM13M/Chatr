43a91dd22f743c62436492a5fba761de092735ec
ShUbHaM13M <shubham.heeralal@gmail.com>
2021-07-01 18:32:16 +0530

    fixes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV !== "production") {
   require('dotenv').config()
 }
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const express = require('express')
 const app = express()
